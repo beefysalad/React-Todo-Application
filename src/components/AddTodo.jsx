52e8b08942fe64aefcd7bf7f00415c44fcd43ea9
John Patrick <jpatrickzephyr@gmail.com>
2022-06-17 14:35:55 +0800

    feat(fix):must have name and description before adding todo

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -4,8 +4,6 @@ export default function AddTodo(props){
     const [todoName,setTodoName] = useState('')
     const [todoDesc,setTodoDesc] = useState('')
     const [index,setIndex] = useState(0)
-    const taskName = useRef(null)
-    const taskDesc = useRef(null)
     const addTodos = ()=>{  
         
         const newTodoObj = {
@@ -23,18 +21,18 @@ export default function AddTodo(props){
     }
 return(
     <div className="add-todos mt-4">
-        <form>
+        <form onSubmit={(event)=>event.preventDefault()}>
             <div className="row text-white">
                 <div className="col-lg-4 mt-2">
                     <label htmlFor="task-name" className="form-label">Task Name</label>
-                    <input maxLength={30} ref={taskName} onChange={(event)=>setTodoName(event.target.value)} value={todoName} id="task-name" placeholder="Task Name" className="custom-input form-control" />
+                    <input maxLength={30} onChange={(event)=>setTodoName(event.target.value)} value={todoName} id="task-name" placeholder="Task Name" className="custom-input form-control" />
                 </div>
                 <div className="col-lg-4 mt-2">
                     <label htmlFor="task-desc" className="form-label">Task Description</label>
-                    <input maxLength={50} ref={taskDesc} onChange={(event)=>setTodoDesc(event.target.value)} value={todoDesc} id="task-desc" placeholder="Description" className="custom-input form-control" />
+                    <input maxLength={50} onChange={(event)=>setTodoDesc(event.target.value)} value={todoDesc} id="task-desc" placeholder="Description" className="custom-input form-control" />
                 </div>
                 <div className="col-lg-4 mt-4 button-side">
-                    <button onClick={addTodos} disabled={todoName<1} className="btn">Add</button>
+                    <button onClick={addTodos} disabled={todoName.length<1 || todoDesc.length<1} className="btn">Add</button>
                     <button onClick={clearTodos} className="btn">Clear</button>
                 </div>
             </div>
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import '../styles/TodoItem.css'
+import {AiFillDelete} from 'react-icons/ai'
 export default function TodoItem(props){
     const renderedTodo = props.todos.map((todo)=>{
         return(
             <div key={todo.index}>
-            {
-                todo.name &&  (<div className="todo-item">
-                    <h2>{todo.name}</h2>
-                    <p>{todo.description}</p>
-                </div>)
-            }
+                {
+                    todo.name &&  (<div className="todo-item">
+                        <h2>{todo.name}</h2>
+                        <p>{todo.description}</p>
+                        
+                    </div>)
+                }
             </div>
         )
     })
